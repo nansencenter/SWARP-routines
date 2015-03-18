f9ec40a7c901ac8321c3d36169c017ea37f665df
gcmdnt90 <gcmdnt90@gmail.com>
2015-03-18 17:23:47 +0100

    18-03 update

--- forecast_scripts/archive_restart.sh
+++ forecast_scripts/archive_restart.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #This is a script to both move and compress the TP4restart files.
 
-#17-03-15 VERSION: creating a TP4restart LIST that will register the name of every restart already archived
+#18-03-15 VERSION: creating a TP4restart LIST that will register the name of every restart already archived
 #I thought that reading through a text file would be faster than checking the whole work folder
 
 rdir=/work/fanf/TOPAZ_RT #restart dir
-cyear=`date -ju "+%Y"` # current year
-bdir=/migrate/timill/restarts/TP4a0.12/SWARP_forecasts/ #backup dir
-mkdir -p $bdir/$cyear # create backup directory for current year if it doesn't exist already
-
+bdir=/migrate/timill/restarts/TP4a0.12/SWARP_forecasts #backup dir
+cyear=`date -u "+%Y"` # current day
 ldir=$bdir/$cyear/log # where to put the log file and file list (one for each year)
 mkdir -p $ldir # create the log dir if it doesn't exist already
 TP4rlog=$ldir/TP4rlog #keeping a log (OK if this is for current year not restart year)
@@ -24,21 +22,18 @@ for el in "${eye[@]}" #analizing one by one the restart files
 	if [ -f $el ] #checking their existence
 	then
                 afil=$(basename $el) # *.a filename without full path
-                base=${afil:0:21}
+                base=${afil%%_mem001.a} #cutting _mem001.a
+		dcre=${base#TP4restart} #cutting TP4restart
                 bfil=$rdir/${base}_mem001.b
                 ufil=$rdir/${base}ICE.uf
-                #
-                ryear=${base:10:4} # want to distinguish current year from year of restart file
-                TP4rlist=$bdir/log/$ryear/TP4rlist #path to the LIST
+                ryear=${dcre%%_*} #keeping the year, want to distinguish between ops year and file year
+                TP4rlist=$bdir/$ryear/log/TP4rlist #path to the LIST
 
-		if grep -Fxq "$el" $TP4rlist #checking their presence in the LIST
+		if grep -Fxq "$base" $TP4rlist #checking their presence in the LIST (we treat the 3 files as a unique set)
 		then
-			echo "$el CHECKED" >> $TP4rlog #file present -> check confirmation (NO NEED TO PUT IN THE LOG, TEMP SAFETY)
+			echo "$base CHECKED" >> $TP4rlog #file present -> check confirmation
 		else 
-			echo "$el" >> $TP4rlist #file not present -> update the LIST
-                        echo "$bfil" >> $TP4rlist
-                        echo "$ufil" >> $TP4rlist
-                        #
+			echo "$base" >> $TP4rlist #file not present -> update the LIST                      
 			baselist="$baselist $base" #adding the file group to the transfer variable
 		fi
 	else
@@ -46,29 +41,21 @@ for el in "${eye[@]}" #analizing one by one the restart files
 		exit
 	fi
 done
-if [ -z "$arch" ] #if arch is empty there are no new restart files
+if [ -z "$baselist" ] #if baselist is empty there are no new restart files
 then
 	echo "NO MODIFICATIONS - ARCHIVE UP TO DATE" >> $TP4rlog
 else
         for base in $baselist
         do
-           afil=$rdir/${base}_mem001.a
-           bfil=$rdir/${base}_mem001.b
-           ufil=$rdir/${base}ICE.uf
-           ryear=${base:10:4} # want to distinguish current year from year of restart file
+           afil=${base}_mem001.a
+           bfil=${base}_mem001.b
+           ufil=${base}ICE.uf
+	   dcre=${base#TP4restart} #cutting TP4restart
+           ryear=${dcre%%_*} #keeping the year, we want to distinguish between ops year and file year
            tfil=${bdir}/$ryear/$base.tar.gz # tar file to create
-           tar -zcvf $tfil $afil $bfil $ufil # TODO remove /work/fanf/TOPAZ_RT from inside tar file
-           #
-           echo "$afil -ADDED" $TP4rlog
-           echo "$bfil -ADDED" $TP4rlog
-           echo "$ufil -ADDED" $TP4rlog
+           tar -zcvf $tfil -C ${rdir} $afil $bfil $ufil #remove /work/fanf/TOPAZ_RT from inside tar file
+           echo "$base set -ADDED" >> $TP4rlog
         done
-	# tar -zcvf ${bdir}/TP4restart_$(date +%Y)_$(date --date yesterday +%j).tar.gz $arch #creating the tar file (HYCOM julian-1 day)
-	# arch=( $arch) #defining as an array
-	# for el in "${arch[@]}"
-	# 	do
-	# 	echo "$el - ADDED" >> $TP4rlog
-	# done
 	echo "FILES ADDED - ARCHIVE UP TO DATE" >> $TP4rlog
 fi
 cat $TP4rlog #printing the log
