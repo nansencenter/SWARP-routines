7794b8617b6506e154ce4156aada689f6708cf0e
tdcwilliams <tdcwilliams@gmail.com>
2015-03-30 13:49:23 +0200

    update 30/03 morning

--- forecast_scripts/process_FCresults.sh
+++ forecast_scripts/process_FCresults.sh
@@ -10,4 +10,20 @@ NCDIR=$RTM/post_processing/archv_netcdf
 FODIR=$RTM/results/TP4a0.12/ice_only/final_output
 WKDIR=$RTM/results/TP4a0.12/ice_only/work
 
+tday=`date +%d_%m_%Y`
+TDIR=$WKDIR/$tday
+mkdir -p $WKDIR/$tday/bin
+mkdir -p $WKDIR/$tday/netcdf
+mkdir -p $WKDIR/$tday/final_output
+mkdir -p $WKDIR/$tday/info
+
+#moving TP4archv & TP4DAILY
+mv $DFDIR/TP4archv* $TDIR/bin
+mv $DFDIR/TP4DAILY* $TDIR/bin
+
+#moving *.nc files
+mv $NCDIR/TP4archv*.nc $TDIR/netcdf
+mv $NCDIR/SWARP*.nc $TDIR/final_output
+
+#
 
