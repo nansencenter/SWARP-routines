d6f833beed26ecac548002744c276139f18f5407
tdcwilliams <tdcwilliams@gmail.com>
2015-04-13 14:32:25 +0200

    rm datefile corrected

--- forecast_scripts/process_FCresults.sh
+++ forecast_scripts/process_FCresults.sh
@@ -6,4 +6,4 @@ $SWARP_ROUTINES/netcdf_production/convert_TP4archv.sh $(cat $datelist | sed '1!d
 $SWARP_ROUTINES/netcdf_production/merge_TP4archv.sh   $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2
 $SWARP_ROUTINES/forecast_scripts/gather_FCresults.sh  $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2 
 $SWARP_ROUTINES/forecast_scripts/collect_FCresults.sh $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2
-rm datelist
+rm $datelist
