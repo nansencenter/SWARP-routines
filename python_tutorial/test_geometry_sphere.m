d02a452310bf3d2e1fc17abf3dc21ca75ca42f57
tdcwilliams <tdcwilliams@gmail.com>
2015-06-03 19:10:50 +0200

    testing of geometry_sphere
    area on an ellipsoid OK
    conformal latitude (+inverse) OK
    TODO great circle distance on ellipsoid (check built-in distance.m)
    (just on sphere now)

---
+++ python_tutorial/test_geometry_sphere.m
@@ -0,0 +1,19 @@
+format long
+
+if 0
+   a              = 6378273;  % semi-major axis
+   ecc            = .1;       % eccentricity of ellipse
+   ellipsoid_mat  = [a,ecc];  % ellipsoid formatted ala matlab
+
+   if 1
+      lons  = [40,50,50,40];
+      lats  = [60,60,70,70];
+   end
+
+   A  = areaint(lats,lons,ellipsoid_mat,'degrees')
+else
+   ecc   = .2;
+   phi   = 70
+   chi   = convertlat([1,ecc], phi, 'geodetic', 'conformal', 'degrees')
+   phi2  = convertlat([1,ecc], chi, 'conformal','geodetic', 'degrees')
+end
