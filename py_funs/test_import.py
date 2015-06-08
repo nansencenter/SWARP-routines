8a5e386807740099b6faf26a3a422d5c86981457
tdcwilliams <tdcwilliams@gmail.com>
2015-06-08 15:35:35 +0200

    test_import.py change
    test imports on hexagon

--- py_funs/test_import.py
+++ py_funs/test_import.py
@@ -6,14 +6,18 @@ from mpl_toolkits.basemap import Basemap, cm
 import time
 from datetime import date, timedelta
 import subprocess
-import pygrib
-from ncepgrib2 import Grib2Encode as g2e
-import shapely.geometry as shgeom # http://toblerity.org/shapely/manual.html
-import rtree.index      as Rindex
-from skimage import measure as msr
+import shapely.geometry as shgeom
 import shapefile
 import glob
 import smtplib
 from email.mime.text import MIMEText
 from scipy.interpolate import griddata as grd
 from operator import itemgetter as itg
+
+# # not present
+# import pygrib
+# from ncepgrib2 import Grib2Encode as g2e # part of pygrib
+# import rtree.index      as Rindex
+# 
+# # errors in import
+# from skimage import measure as msr # no measure in skimage?
