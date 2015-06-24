11a1faa629a7d57d6ca18f15dc5c17437cfae8ce
tdcwilliams <tdcwilliams@gmail.com>
2015-06-24 14:49:37 +0200

    comments to Laplace eqn

--- py_funs/Laplace_eqn_solution.py
+++ py_funs/Laplace_eqn_solution.py
@@ -560,11 +560,12 @@ class dirichlet_fund_soln:
          pobj.pcolor(xp,yp,Fm,vmin=vmin,vmax=vmax,cmap=cmap)
          pobj.colorbar()
          pobj.contour(X/1.e3,Y/1.e3,F,vlev,colors='k')
-         #
+
+         # plot polygon boundary
          x,y   = GP.coords2xy(self.coords)
          pobj.plot(x/1.e3,y/1.e3,'k',linewidth=2)
 
-         #
+         # plot singularities
          x,y   = GP.coords2xy(self.singularities)
          pobj.plot(x/1.e3,y/1.e3,'.k',markersize=5)
          pobj.xlabel('x, km')
