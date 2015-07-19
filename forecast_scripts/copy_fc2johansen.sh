424338c3ccb3b738a3efbd04d70cd17ec77b4874
tdcwilliams <timill@johansen.ad.nersc.no>
2015-07-19 13:19:57 +0200

    move from "hidden" directory - source ssh_info.src in hidden

---
+++ forecast_scripts/copy_fc2johansen.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# copy forecast file from hexagon to johansen
+
+# final destination of forecast outputs
+THREDDS="/mnt/10.11.12.232/Projects/SWARP/Thredds_data_dir"
+# location of SWARP routines on johansen
+SRjoh="/Data/sim/tim/Projects/SWARP/SWARP-routines"
+# location of SWARP routines on hexagon
+SRhex="/home/nersc/timill/GITHUB-REPOSITORIES/SWARP-routines"
+# location of forecast outputs on hexagon
+TP4rt="/work/timill/RealTime_Models/results/TP4a0.12"
+
+joh_dir=$THREDDS/ice_only #directory on johansen where final file should go
+joh_dir_w=$THREDDS/wavesice #directory on johansen where final file should go
+tmp_dir=$THREDDS/ice_only/tmp #directory on johansen where final file should go
+tmp_dir_w=$THREDDS/wavesice/tmp #directory on johansen where final file should go
+hex_dir=$TP4rt/ice_only/work # dir on hexagon where final file ends up
+hex_dir_w=$TP4rt/wavesice/work # dir on hexagon where final file ends up
+wrk_dir=$SRjoh/forecast_scripts/hidden # location of logs, ssh_info.src
+# fc_dir=$SRhex/forecast_scripts
+
+mkdir -p $tmp_dir
+mkdir -p $tmp_dir_w
+
+# keyname=key_johansen2hexagon
+# user=timill
+
+# get $keyname, $user
+source $wrk_dir/ssh_info.src
+
+# scp -i $HOME/.ssh/$keyname $user@hexagon.bccs.uib.no:$fc_dir/fc_alert_email.txt $tmp_dir
+cp $wrk_dir/../fc_alert_email.txt $tmp_dir
+
+# EMAIL ADRESS
+# ==================================================================================
+email=$(cat $tmp_dir/fc_alert_email.txt)
+# ==================================================================================
+
+cplog=$wrk_dir/cplog
+tday=$(date +%Y%m%d)
+
+if [ -f $cplog ] && [ $tday != $(cat $cplog | sed '1!d') ]
+then
+   mv $cplog $wrk_dir/old_logs/$(cat $cplog | sed '1!d')
+fi
+
+if [ $(date +%A) == "Monday" ]
+then
+   rm $wrk_dir/old_logs/*
+fi
+
+if [ ! -f "$cplog" ]
+then
+   touch $cplog
+   echo $tday >> $cplog
+fi
+
+wrn_count=0
+
+# finding the latest final product - WAVESICE
+# - check last 4 days
+for n in {0..4}
+do
+   echo "$(date +%H:%M) - looking for WAVESICE latest file" >> $cplog
+   hdate=$(date --date="$n days ago" '+%Y%m%d')
+   echo "Looking for product $hdate" >> $cplog
+   hex_fil=SWARPwavesice_forecast_start${hdate}*  # final file
+
+   echo scp -i $HOME/.ssh/$keyname $user@hexagon.bccs.uib.no:$hex_dir_w/$hdate/final_output/$hex_fil $tmp_dir_w
+   scp -i $HOME/.ssh/$keyname $user@hexagon.bccs.uib.no:$hex_dir_w/$hdate/final_output/$hex_fil $tmp_dir_w
+
+   if [ -f $tmp_dir_w/$hex_fil ]
+   then
+      mv $tmp_dir_w/* $joh_dir_w/
+      chmod o+r $joh_dir_w/$hex_fil
+      echo "Product found on $hdate!" >> $cplog
+      echo "Latest product uploaded" >> $cplog
+      echo "" >> $cplog
+      break
+   else
+      echo "No product on $hdate" >> $cplog
+      wrn_count=$(expr $wrn_count + 1)
+      if [ "$wrn_count" -gt 2 ]
+      then
+         echo ""                          >> $cplog
+         echo "PRODUCT OLDER THAN 3 DAYS" >> $cplog
+         mail -s "WARNING - Johansen Old Product" $email < $cplog
+         break
+      fi
+   fi
+done
+
+wrn_count=0
+
+# finding the latest final product - ICE_ONLY
+# - check last 4 days
+for n in {0..4}
+do
+   echo "$(date +%H:%M) - looking for ICE_ONLY latest file" >> $cplog
+   hdate=$(date --date="$n days ago" '+%Y%m%d')
+   echo "Looking for product $hdate" >> $cplog
+   hex_fil=SWARPiceonly_forecast_start${hdate}*  # final file
+
+   echo scp -i $HOME/.ssh/$keyname $user@hexagon.bccs.uib.no:$hex_dir/$hdate/final_output/$hex_fil $tmp_dir
+   scp -i $HOME/.ssh/$keyname $user@hexagon.bccs.uib.no:$hex_dir/$hdate/final_output/$hex_fil $tmp_dir
+
+   if [ -f $tmp_dir/$hex_fil ]
+   then
+      mv $tmp_dir/* $joh_dir/
+      chmod o+r $joh_dir/$hex_fil
+      echo "Product found on $hdate!" >> $cplog
+      echo "Latest product uploaded" >> $cplog
+      echo "" >> $cplog
+      break
+   else
+      echo "No product on $hdate" >> $cplog
+      wrn_count=$(expr $wrn_count + 1)
+      if [ "$wrn_count" -gt 2 ]
+      then
+         echo ""                          >> $cplog
+         echo "PRODUCT OLDER THAN 3 DAYS" >> $cplog
+         mail -s "WARNING - Johansen Old Product" $email < $cplog
+         break
+      fi
+   fi
+done
+
+# make key with:
+# ssh-keygen -t dsa $HOME/.ssh/$keyname
+# copy $HOME/.ssh/$keyname.pub to hexagon
+# paste contents of $HOME/.ssh/$keyname to the bottom of ~/.ssh/authorised_keys (on target - hexagon)
