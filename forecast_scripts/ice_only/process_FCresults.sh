ce847317dd6f1953df5d185ab76c6e69da5ccf6f
tdcwilliams <tdcwilliams@gmail.com>
2015-07-23 15:00:50 +0200

    revert to YYY-MM-DDTHH:MM:SSZ date convention for post-processing of ice_only FC - this is the standard

--- forecast_scripts/ice_only/process_FCresults.sh
+++ forecast_scripts/ice_only/process_FCresults.sh
@@ -10,12 +10,20 @@ email="gcmdnt90@gmail.com"
 datelist=$SWARP_ROUTINES/forecast_scripts/datelist.txt
 if [ -f $datelist ]
 then
-   $SWARP_ROUTINES/forecast_scripts/ice_only/gather_FCresults.sh  $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2 
-   $SWARP_ROUTINES/netcdf_production/convert_TP4archv.sh $(cat $datelist | sed '1!d') #$1
-   $SWARP_ROUTINES/netcdf_production/merge_TP4archv.sh   $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2
-   $SWARP_ROUTINES/forecast_scripts/ice_only/collect_FCresults.sh $(cat $datelist | sed '1!d') $(cat $datelist | sed '2!d') #$1 $2
-   cp $datelist /work/timill/RealTime_Models/results/TP4a0.12/ice_only/work/$(cat $datelist | sed '1!d')/info/
+   # set var's
+   tday=$(cat $datelist | sed '1!d')      # first day in YYYYMMDD format
+   # tday_long=$(cat $datelist | sed '2!d') # first day in YYYY-MM-DD format TODO shouldn't need this (just convert $tday)
+
+   # run scripts
+   $SWARP_ROUTINES/forecast_scripts/ice_only/gather_FCresults.sh  $tday # $day1_long #$1 $2 
+   $SWARP_ROUTINES/netcdf_production/convert_TP4archv.sh $tday #$1
+   $SWARP_ROUTINES/netcdf_production/merge_TP4archv.sh   $tday # $day1_long #$1 $2
+   $SWARP_ROUTINES/forecast_scripts/ice_only/collect_FCresults.sh $tday # $day1_long #$1 $2
+
+   # add datelist.txt to info
+   cp $datelist /work/timill/RealTime_Models/results/TP4a0.12/ice_only/work/$tday/info/
 else
+   # send alert email
    touch log.txt
    echo "DATELIST NOT FOUND" >> log.txt
    mail -s "Process forecast problems" $email < log.txt
