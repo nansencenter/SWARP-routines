9307b2e1ce97f6237bb24b417960462c0e11b21d
tdcwilliams <tdcwilliams@gmail.com>
2015-09-10 22:27:43 +0200

    minor changes to test_import.py

--- py_funs/test_import.py
+++ py_funs/test_import.py
@@ -1,4 +1,3 @@
-from netCDF4 import Dataset
 import sys,os
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,17 +6,23 @@ import time
 from datetime import date, timedelta
 import subprocess
 import shapely.geometry as shgeom
-import shapefile
+import shapefile #(pyshp package)
 import glob
 import smtplib
 from email.mime.text import MIMEText
 from scipy.interpolate import griddata as grd
 from operator import itemgetter as itg
 
-# # not present
-# import pygrib
-# from ncepgrib2 import Grib2Encode as g2e # part of pygrib
-# import rtree.index      as Rindex
+# not present (2.7.2)
+import pygrib
+from ncepgrib2 import Grib2Encode as g2e  # part of pygrib package
+# import rtree.index      as Rindex         # not originally requested 
 # 
-# # errors in import
-# from skimage import measure as msr # no measure in skimage?
+# errors in import (2.7.2)
+from skimage import measure as msr
+# 
+# # errors in import (2.7.9)
+from netCDF4 import Dataset
+
+# # not present (2.7.9)
+import rtree.index      as Rindex         # not originally requested 
