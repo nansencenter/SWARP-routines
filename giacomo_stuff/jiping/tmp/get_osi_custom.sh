7e1375173f4c72a07d01738c0675348622613f65
tdcwilliams <tdcwilliams@gmail.com>
2015-11-13 17:33:50 +0100

    bugfix

--- giacomo_stuff/jiping/tmp/get_osi_custom.sh
+++ giacomo_stuff/jiping/tmp/get_osi_custom.sh
@@ -7,20 +7,11 @@ year=${target:0:4}
 month=${target:4:2}
 day=${target:6:2}
 
-# where the conc files are stored
-tmp_dir=/Documents/SWARP-routines/giacomo_stuff/jiping/tmp
-cd $tmp_dir
-
 # where the files are stored in the myocean portal 
 mo_dir='SIW-TAC/SIW-OSISAF-GLO-SIT_SIE_SIC-OBS/conc/'
 mo_dir=$mo_dir/$year/$month
-for nday in `seq -w 01 $day`
-do
-   fname=ice_conc_nh_polstere-100_multi_$year$month${nday}1200.nc
-   if [ ! -f $fname ]
-   then
-      # if file isn't there, get from myocean portal
-      LIST="$mo_dir/$fname"
+fname=ice_conc_nh_polstere-100_multi_$year$month${nday}1200.nc
+LIST="$mo_dir/$fname"
 
 #########################################################################
 # get the file
