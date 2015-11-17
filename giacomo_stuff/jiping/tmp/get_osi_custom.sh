390f499fa161fdf3df7f3628d59637e17d410c8a
tdcwilliams <tdcwilliams@gmail.com>
2015-11-17 16:49:09 +0100

    donwload old osi

--- giacomo_stuff/jiping/tmp/get_osi_custom.sh
+++ giacomo_stuff/jiping/tmp/get_osi_custom.sh
@@ -8,7 +8,7 @@ month=${target:4:2}
 day=${target:6:2}
 
 # where the files are stored in the myocean portal 
-mo_dir='SIW-TAC/SIW-OSISAF-GLO-SIT_SIE_SIC-OBS/conc/'
+mo_dir='SIW-TAC/SIW-OSISAF-GLO-SIT_SIE_SIC-OBS/conc'
 mo_dir=$mo_dir/$year/$month
 fname=ice_conc_nh_polstere-100_multi_$year$month${nday}1200.nc
 LIST="$mo_dir/$fname"
@@ -17,16 +17,12 @@ LIST="$mo_dir/$fname"
 # get the file
 # (can't handle too many at once it seems)
 # make a text file (between "<<EOF" and "EOF")
-  cat > ncftp.in<<EOF
+cat > ncftp.in<<EOF
 open myocean 
 get $LIST
 set confirm-close no
 bye
 EOF
-         ncftp < ncftp.in # text file passd into ncftp
-         rm ncftp.in
+ncftp < ncftp.in # text file passd into ncftp
+rm ncftp.in
 #########################################################################
-
-   fi
-
-done
