713a093db95d6ee93ed106ea88147b5b5506a0f7
tdcwilliams <tdcwilliams@gmail.com>
2015-12-01 16:14:05 +0100

    TW: ice_only FC restructured

---
+++ forecast_scripts/ModelSetups/common/post/convert_archv.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Runs hyc2proj on all files of archv type
+
+# ===================================================================================
+source $SWARP_ROUTINES/source_files/hex_vars.src
+THIS_SRC=`readlink -f $1`
+source $THIS_SRC
+# ===================================================================================
+
+# ==================================================================================
+# EMAIL ADDRESS
+email=$(cat $FCemail)
+# ==================================================================================
+
+# In .bash_profile:
+# 1. add MSCPROGS/bin to $PATH variable
+#    (may need to compile MSCPROGS with "make" & "make install"
+# 2. add this directory to $PATH variable
+# 3. set environment variable $GIT_REPOS to the directory containing
+#     this repository ("SWARP-routines")
+
+# Info for hyc2proj
+# - copied from MSCPROGS/Input & edited
+h2p_in="$SWARP_ROUTINES/netcdf_production/Input"
+
+# make a "working" directory
+tday=$2
+sdir=$THISFC2/$tday/netcdf
+ddir=$THISFC2/$tday/bin
+mkdir -p $sdir
+
+cd $sdir
+mkdir -p tmp
+log=$sdir/tmp/convert_log.txt
+touch $log
+cd tmp
+
+# Info for hyc2proj
+ln -s $h2p_in/proj.in .
+ln -s $h2p_in/extract.archv . 
+ln -s $h2p_in/extract.daily . 
+ln -s $h2p_in/depthlevels.in .
+
+# Info about grid from topo dir
+topo_input=$RTmods/$HYCOMreg/topo
+ln -s $topo_input/grid.info .
+ln -s $topo_input/regional.grid.a .
+ln -s $topo_input/regional.grid.b .
+ln -s $topo_input/regional.depth.a .
+ln -s $topo_input/regional.depth.b .
+
+# CONVERT ARCH
+if [ $(ls $ddir | tail -1) ]
+then
+   for f in $ddir/${rungen}archv*.a
+      do
+         len=${#ddir}
+         g=${f:$((len+1))}
+         len=${#g}
+         base=${g:0:$((len-2))}
+         ln -s $f $ddir/$base.b .
+
+         echo "********************************************************"
+         echo "Running hyc2proj on $g..."
+         echo "********************************************************"
+         echo " "
+
+         echo hyc2proj $g
+         echo cp *.nc $sdir
+         echo " "
+         hyc2proj $g
+         cp *.nc $sdir
+      done
+else
+   "No archv* files in $ddir" >> $log
+   mail -s "convert_archv FAILED" $email < $log
+fi
+
+# CONVERT DAILY
+if [ $(ls $ddir | tail -1) ]
+then
+   for f in $ddir/${rungen}DAILY*.a
+      do
+         len=${#ddir}
+         g=${f:$((len+1))}
+         len=${#g}
+         base=${g:0:$((len-2))}
+         ln -s $f $ddir/$base.b .
+
+         echo "********************************************************"
+         echo "Running hyc2proj on $g..."
+         echo "********************************************************"
+         echo " "
+
+         echo hyc2proj $g
+         echo cp *.nc $sdir
+         echo " "
+         hyc2proj $g
+         cp *.nc $sdir
+      done
+else
+   "No DAILY* files in $ddir" >> $log
+   mail -s "convert_archv FAILED" $email < $log
+fi
+
+cd $sdir
+rm -r tmp
+
+echo "********************************************************"
+echo "Netcdf files in $sdir:"
+ls -lh
+echo "********************************************************"
--- forecast_scripts/ModelSetups/common/post/merge_archv.sh
+++ forecast_scripts/ModelSetups/common/post/merge_archv.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # script to extract some variables from a netcdf file
 
+# NO NEED FOR A MAIL ALERT. IF CONVERT WORKS THEN THIS WILL AS WELL. IF CONVERT FAILS THEN IT WILL SEND A MESSAGE.
+
+# ===================================================================================
 source $SWARP_ROUTINES/source_files/hex_vars.src
 THIS_SRC=`readlink -f $1`
 source $THIS_SRC
-
 # ===================================================================================
+
+# ==================================================================================
 # EMAIL ADDRESS
 email=$(cat $FCemail)
-# ===================================================================================
+# ==================================================================================
 
 #########################################################################
 # inputs are start date of forecast
@@ -18,49 +22,46 @@ then
    tday=`date +%Y%m%d`                       # start date of forecast YYYYMMDD
    echo \${tday}=$tday
    tday_long=`date --date=$tday +%Y-%m-%d`   # start date of forecast YYYY-MM-DD
-   lognm=merge_wav_log_test.txt
+   lognm=merge_log_test.txt
 else
    tday=$2                                   # start date of forecast YYYYMMDD
    tday_long=`date --date=$tday +%Y-%m-%d`   # start date of forecast YYYY-MM-DD
-   # tday_long=`date --date="$2" +%Y-%m-%d`  # start date of forecast YYYY-MM-DD
-   lognm=merge_wav_log.txt
+   lognm=merge_log.txt
 fi
-#########################################################################
-
-#########################################################################
 dir0=$THISFC2/$tday/netcdf
-odir=$THISFC2/$tday/final_output  
+odir=$THISFC2/$tday/final_output
 #########################################################################
-
-#extract start date/time of forecast
-# and put it into output filename
 
 cd $dir0
-mkdir -p tmp
+rm -r -f  tmp
+mkdir tmp
 
 # LOG
-logdir=$THISFC/logs
-log=$logdir/$lognm
+log=$THISFC/logs/$lognm
 if [ -f "$log" ]
 then
    rm $log
 fi
 touch $log
 
-echo $date                                            >> $log
 echo " "                                              >> $log
 echo "Unpacking files (ncpdq -U)..."                  >> $log
 
+flist=(*.nc)
+f=${flist[0]}
+
 Nfiles=0
-for f in ${rungen}archv_wav*.nc
+for f in ${rungen}archv_*.nc
 do
-   end=${f#*dump}
-   fdate=${end:0:8}
-   if [ $fdate -ge $tday ]
+   cf=${f:9:8}
+   if [ "$cf" -ge "$tday" ]
    then
       Nfiles=$((Nfiles+1))
       echo Unpacking   $f
+      # unpack files to make sure that scale factors are same in each file
       ncpdq -U $f tmp/$f
+   else
+      echo "*.nc older than actual date"              >> $log
    fi
 done
 
@@ -85,8 +86,7 @@ ncrename -v fice,icec   $ofil #ice cover
 ncrename -v hice,icetk  $ofil #ice thickness
 
 # change time:units
-ncatted -O -h -a units,time,m,c,"seconds since 1970-01-01T00:00:00Z" $ofil
-#########################################################################
+ncatted -O -h -a units,time,m,c,"seconds since 1970-01-01T00:00:00Z"  $ofil
 
 ###########################################################################################
 # most variable attributes set in hyc2proj:
@@ -157,9 +157,8 @@ ncatted -O -h -a project,global,c,c,"SWARP"                                   $o
 ncatted -O -h -a project_references,global,c,c,"swarp.nersc.no"               $ofil
 ncatted -O -h -a distribution_statement,global,c,c,"No restrictions"          $ofil
 ncatted -O -h -a operational_status,global,c,c,"test"                         $ofil
-
-# wave forcing info
 #
+ncatted -O -h -a title,global,o,c,"SWARP sea ice forecast"               $ofil # o=overwrite/create, c=format (also f=float)
 # ncatted -O -h -a history,global,o,c,"NERSC-HYCOM output->hyc2proj->ncrcat"    $ofil
 
 # Restart file date
@@ -169,13 +168,13 @@ then
    # reformat bulletin date
    # eg 2015-05-09T00:00:00Z -> 2015-05-09 UTC 00:00:00
    bdate_info=$(ncinfo $ofil | grep "bulletin_date")
-   split=(${bdate_info//\ / }) # make array with delimiter space "\ "
-   bdate_time=${split[1]}       # eg 2015-05-09T00:00:00Z
-   split=(${bdate_time//T/ }) # make array with delimiter "T" 
-   bdate=${split[0]}               # eg 2015-05-09
-   btime=${split[1]}               # eg 00:00:00Z
-   btime=${btime:0:8} # remove "Z"
-   ncatted -O -h -a bulletin_date,global,o,c,"$bdate UTC $btime" $ofil   # add new attribute "restart_file_date"
+   split=(${bdate_info//\ / })   # make array with delimiter space "\ "
+   bdate_time=${split[1]}        # eg 2015-05-09T00:00:00Z
+   split=(${bdate_time//T/ })    # make array with delimiter "T"
+   bdate=${split[0]}             # eg 2015-05-09
+   btime=${split[1]}             # eg 00:00:00Z
+   btime=${btime:0:8}            # remove "Z"
+   ncatted -O -h -a bulletin_date,global,o,c,"$bdate UTC $btime" $ofil
 fi
 
 # Rename bulletin_date to restart_file_date (clearer)
@@ -201,16 +200,16 @@ Ncorrect=$FCrecords
 if [ $Nfiles -ne $Ncorrect ]
 then
    efil=swarp_tmp.txt
-   echo Warning: merge_archv_wav.sh                            >  $efil
+   echo Warning: merge_archv.sh                                >  $efil
    echo Wrong number of records in $ofil                       >> $efil
    echo "($Nfiles -  should be $Ncorrect)"                     >> $efil
    mail -s "WARNING: $FCtype_long final product faulty" $email <  $efil
    rm $efil
 else
    efil=swarp_tmp.txt
-   echo Confirmation: merge_archv_wav.sh                       >  $efil
+   echo Confirmation: merge_TP4archv.sh                        >  $efil
    echo "Correct number of records ($Ncorrect) in $ofil"       >> $efil
-   mail -s "$FCtype_long final product OK" $email              <  $efil
+   mail -s "$FCtype_long final product OK" $email              < $efil
    rm $efil
 fi
 ###########################################################################################
