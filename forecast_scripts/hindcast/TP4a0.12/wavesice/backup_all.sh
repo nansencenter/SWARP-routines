b1c9fa2a89079074140afdb63db9c4ae5d87c2e6
tdcwilliams <tdcwilliams@gmail.com>
2016-02-04 14:02:11 +0100

    TW: backup scripts for hindcasts

---
+++ forecast_scripts/hindcast/TP4a0.12/wavesice/backup_all.sh
@@ -0,0 +1,49 @@
+# backup all hindcasts:
+source $SWARP_ROUTINES/source_files/hex_vars.src
+
+R=TP4a0.12
+rungen=${R:0:3}
+HCtype=wavesice
+HCtype2=$HCtype
+#
+print_info=1
+#
+hd=$SWARP_ROUTINES/forecast_scripts/hindcast/$R
+user=timill #TODO source hidden
+hd2=$hd/$HCtype
+outdir0=$TP4_REALTIME/../results_hindcasts/$R/$HCtype
+
+# location on migrate
+migdir0=/migrate/timill/RESULTS/$R/SWARP_hindcasts
+mkdir -p $migdir0
+migdir0=$migdir0/$HCtype
+mkdir -p $migdir0
+
+
+for YEAR in 2015 2016
+do
+   outdir=$outdir0/${YEAR}_GOOD
+   cd $outdir
+
+   fp=final_output_${HCtype}_${YEAR}
+   mkdir -p $fp
+
+   migdir=$migdir0/$YEAR
+   mkdir -p $migdir
+
+   for subdir in ${YEAR}_*
+   do
+      echo $subdir
+      chmod -R g+r $subdir
+      cp $subdir/final_output/*.nc $fp
+      tfil=$subdir.tar.gz 
+      tar -zcvf $tfil $subdir
+      cp $tfil $migdir
+      echo " "
+   done
+
+   chmod -R g+r $fp
+   chmod -R o+r $fp
+   Tfil=${fp}.tar.gz
+   tar -zcvf $Tfil $fp
+done
