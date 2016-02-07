cf2183e4fd4a2db7ee032057ed4640931ece6c94
tdcwilliams <tdcwilliams@gmail.com>
2016-02-07 19:56:58 +0100

    hex to local

--- giacomo_hexagon/ice_edge_validation/run_ice_edge_analysis.sh
+++ giacomo_hexagon/ice_edge_validation/run_ice_edge_analysis.sh
@@ -14,76 +14,18 @@ osidir=/work/shared/nersc/msc/OSI-SAF
 wrkdir=/home/charlie/Documents/SWARP-routines/giacomo_stuff/jiping
 results=/home/nersc/timill/GITHUB-REPOSITORIES/SWARP-routines/validation/outputs
 
-startdate='20150601'
-enddate='20150630'
+startdate='19910101'
+enddate='20141231'
 sdj=$(date --date="$startdate" +%j)
 edj=$(date --date="$enddate" +%j)
 ndays=$(expr $edj - $sdj)
 
 for n in $(seq 0 $ndays)
 do
-   # Cleaning the data dir
-   rm ${wrkdir}/OSI/*
-   rm ${wrkdir}/MDL/*
-   
-   # Downloading the data
    hdate=$(date --date="$startdate + $n days" '+%Y%m%d')
    jdate=$(date --date="$hdate" '+%j')
    jdate=$(expr $jdate - 1)
    echo $jdate
    year=${hdate:0:4}
-   o_obj=${osidir}/${year}_nh_polstere/ice_conc_nh_polstere-100_multi_${hdate}1200.nc
-   m_obj=${mdldir}/${year}/SWARP_wavesice_forecast_${hdate}.tar.gz 
-   if [ -f ${o_obj} ]
-   then
-      echo "Found --> ${hdate}"
-      cp ${o_obj} ${wrkdir}/OSI/
-   else   
-      echo "NOT found --> ${hdate}"
-   fi
-   echo ""
-   echo "Osisaf ${hdate} COMPLETE"
-   echo ""
-   if [ -f ${m_obj} ]
-   then
-      echo "Found --> ${hdate}"
-      echo ""
-#      # Binary extraction
-#      steve=${hdate}/bin/TP4archv_wav.${year}_${jdate}_120000.a
-#      alan=${hdate}/bin/TP4archv_wav.${year}_${jdate}_120000.b
-#      echo "Unpacking..."
-#      tar -zxvf ${m_obj} ${steve} || echo "Binary .a not present" 
-#      tar -zxvf ${m_obj} ${alan} || echo "Binary .b not present" 
-#      echo "Done."
-#      mv ${hdate} ${wrkdir}/MDL
-#      mv ${wrkdir}/MDL/${hdate}/bin/* ${wrkdir}/MDL
-#      rm -rf ${wrkdir}/MDL/${hdate}
-      # NetCDF extraction
-      steve=${hdate}/netcdf/TP4archv_wav_start${hdate}_000000Z_dump${hdate}_120000Z.nc
-      echo "Unpacking..."
-      tar -zxvf ${m_obj} ${steve} || echo "NetCDF not present"
-      echo "Done."
-      mv ${hdate} ${wrkdir}/MDL
-      mv ${wrkdir}/MDL/${hdate}/netcdf/* ${wrkdir}/MDL
-      rm -rf ${wrkdir}/MDL/${hdate}
-   else   
-      echo "NOT found --> ${hdate}"
-   fi
-   echo ""
-   echo "Model ${hdate} COMPLETE"
-   echo ""
-   if [ -f ${wrkdir}/OSI/* ] && ([ -f ${wrkdir}/MDL/*.a ] || [ -f ${wrkdir}/MDL/*.nc ])
-   then
-      # for ICP
-      #python time_series_miz.py "ICP" "${hdate}"
-      #mv -f ${results}/ICP/* /work/users/timill/RESULTS/POLYGONS/ICP
-      # for DFP
-      python time_series_miz.py "DFP" "${hdate}"
-      mv -f ${results}/DFP/* /work/users/timill/RESULTS/POLYGONS/DFP
-      # for AOD
-      python time_series_miz.py "AOD" "${hdate}"
-      mv -f ${results}/AOD/* /work/users/timill/RESULTS/POLYGONS/AOD
-   else
-      echo "Data not available"
-   fi
+   python ice_edge_analysis.py "arctic" "${hdate}" "./outputs"
 done
