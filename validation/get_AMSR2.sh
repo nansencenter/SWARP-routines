f6acaa45371100c28086c7924301554a016f5843
tdcwilliams <tdcwilliams@gmail.com>
2016-02-08 16:19:04 +0100

    script to download AMSR2 conc daily

---
+++ validation/get_AMSR2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# each day this script gets yesterday's AMSR2 concentration
+# - last update is around 2000
+
+# get yesterday's date
+yyear=`date -d "yesterday" '+%Y'`
+ymon=`date -d "yesterday" '+%m'`
+yday=`date -d "yesterday" '+%d'`
+ydate=$yyear$ymon$yday
+gfil=Arc_${ydate}_res3.125_pyres.nc.gz
+ftpdir="ftp://ftp-projects.zmaw.de/seaice/AMSR2/3.125km/"
+
+# where the conc files are stored
+hex_dir=/work/shared/nersc/msc/AMSR2_3125
+cd $hex_dir
+
+mkdir -p tmp
+cd tmp
+wget $ftpdir/$gfil
+gunzip $gfil
+mv *.nc ..
+cd ..
+rm -r tmp
