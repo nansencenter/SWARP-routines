3a3786c8da8939906866f096e12068f4b3c2da02
tdcwilliams <tdcwilliams@gmail.com>
2016-10-24 12:17:01 +0200

    bug fix in DMI_process.sh

--- validation/ice_charts/DMI_process.sh
+++ validation/ice_charts/DMI_process.sh
@@ -8,7 +8,7 @@ else
 fi
 
 indir=`pwd`
-outdir=/work/shared/nersc/msc/DMI_icecharts/sigrid/$cyear
+outdir=/work/shared/nersc/msc/DMI_icecharts/MIZ_FA
 
 ME=`readlink -f $0`
 Vdir=`dirname $ME`
@@ -16,10 +16,11 @@ Vdir=`dirname $ME`
 # load python
 [ -f /etc/bash.bashrc ] && . /etc/bash.bashrc
 module load python/2.7.9-dso
+module load geos
 export PYTHONPATH=$PYTHONPATH:$SWARP_ROUTINES/py_funs
 
 # extract MIZ from ice chart
 pyscript=$Vdir/read_icechart.py
-$python --indir=$indir --outdir=$outdir --chart_source=DMI --MIZ_criteria="FA_only" --overwrite=False
+$python $pyscript --indir=$indir --outdir=$outdir --chart_source=DMI --MIZ_criteria="FA_only" --overwrite=False
 
 # TODO MIZ width/area?
