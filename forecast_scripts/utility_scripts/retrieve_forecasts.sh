265bf1b1e7f3d5f14b26ceb39ffd26cbb3f48fc4
tdcwilliams <tdcwilliams@gmail.com>
2017-03-10 08:33:56 +0100

    Scripts to clean old forecasts and retrieve them from migrate

---
+++ forecast_scripts/utility_scripts/retrieve_forecasts.sh
@@ -0,0 +1,56 @@
+me=$0
+if [ $# -ne 2 ]
+then
+   echo "`basename $me` [region] [FCtype]"
+   exit
+fi
+reg=$1
+FCtype=$2
+
+if [ $reg == "TP4" ]
+then
+   wdir=$TP4_REALTIME_RES/$FCtype
+   mdir=/migrate/timill/RESULTS/TP4a0.12/SWARP_forecasts/$FCtype
+fi
+
+cd $wdir
+for Ydir in $mdir/*
+do
+   # yearly directory
+   year=`basename $Ydir`
+   if [ $year -lt 2016 ]
+   then
+      echo Skipping $year
+      continue
+   fi
+
+   for Gfil in $Ydir/*
+   do
+      # loop over daily forecasts
+      gfil=`basename $Gfil`
+      gg=${gfil%*.tar.gz}
+      N=${#gg}
+      cdate=${gg:$((N-8)):$N}
+      echo $cdate
+
+      if [ -d $cdate ]
+      then
+         # don't overwrite directories
+         echo Skipping $cdate
+         continue
+      else
+         echo Getting $cdate
+         # continue
+         mkdir $cdate
+         cd $cdate
+      fi
+
+      echo cp $Gfil .
+      cp $Gfil .
+
+      echo tar -zxf $gfil
+      tar -zxf $gfil
+      rm $gfil
+      cd $wdir
+   done
+done
